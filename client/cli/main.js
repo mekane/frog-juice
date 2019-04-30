d4c795bbc444b6531a68657c2090d3d76f4b08b5
Marty Kane <marty.kane@gmail.com>
2019-04-30 20:03:40 +0000

    Add skeleton client logic: main game loop and final scores output

--- client/cli/main.js
+++ client/cli/main.js
@@ -6,10 +6,11 @@ const numberOfPlayers = getNumberOfPlayers();
 
 console.log(`Beginning new game with ${numberOfPlayers} players`);
 
+game.newGame(numberOfPlayers);
 
+gameLoop();
 
-
-
+displayFinalScores();
 
 
 function getNumberOfPlayers() {
@@ -17,4 +18,19 @@ function getNumberOfPlayers() {
     if (isNaN(num) || typeof num !== 'number')
         return 2;
     return Math.min(Math.max(num, 2), 4);
+}
+
+function gameLoop() {
+    //show state
+    //prompt current player for input (depending on phase)
+}
+
+function displayFinalScores() {
+    const scores = game.getPlayerScores();
+    const player = game.currentState().players.byId;
+
+    console.log(`\nGAME OVER`);
+    for (let i = 0; i < scores.length; i++) {
+        console.log(`${player[i].name}: ${scores[i]}`);
+    }
 }
